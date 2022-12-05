81b2390b98cd00f9236fc2759f5b891b8dd27100
Tjan1Kajba <119865245+Tjan1Kajba@users.noreply.github.com>
2022-12-05 19:00:31 +0100

    Add files via upload

---
+++ linux6.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+folders='Desktop Documents Downloads Pictures Videos'
+for folder in $folders
+do
+    mkdir $folder
+    echo "Ustvarjena mapa $folder"
+done
+
+
+for i in {1..5}
+do
+    mkdir "folder$i"
+    echo "Ustvarjena mapa folder$i"
+done
+
+
+wget -q -O users.txt "https://github.com/Tjan1Kajba/linux6/blob/main/uporabniki.txt"
+input=users.txt
+while IFS= read -r username
+do
+        
+    if grep $username /etc/passwd > /dev/null 
+    then
+        echo "Uporabnik $username že obstaja"
+    else    
+        useradd -G sudo -m $username
+        echo "Ustvarjen uporabnik: $username"
+    fi
+done < "$input"
+
+
+if apt-get update
+then
+    apt-get upgrade -y
+fi
+
+
+apt-get install -y ufw git nginx net-tools
+if apt-get install -y ca-certificates curl gnupg lsb-release
+then
+    mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+fi
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+echo "Konec :)"
